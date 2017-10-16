d2792fa724d05d5cf54a1bee01b0a3e411bcf9ea
zhangchi <zhangchi@babytree-inc.com>
2017-10-16 17:45:22 +0800

    first

---
+++ Tpod2/Tpod2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tpod2
+//
+//  Created by zhangchi on 2017/10/16.
+//  Copyright © 2017年 zhangchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tpod2/Tpod2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tpod2
+//
+//  Created by zhangchi on 2017/10/16.
+//  Copyright © 2017年 zhangchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
